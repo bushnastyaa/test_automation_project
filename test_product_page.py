9172085236b5c30447fe691ec8d3668b7e8353f3
Nastya <bushnastyaa@mail.ru>
2023-01-31 20:09:17 +0300

    Add login class in test pages

--- test_product_page.py
+++ test_product_page.py
@@ -48,18 +48,18 @@ def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page.success_message_should_disappeared()
 
 
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+class TestCanGoToLoginPageFromProductPage():
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
-
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
 
 
 @pytest.mark.go_to_basket
