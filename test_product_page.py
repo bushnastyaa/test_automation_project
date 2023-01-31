242395780fc63f14c0a6b33974d06085714f8913
Nastya <bushnastyaa@mail.ru>
2023-01-31 22:14:49 +0300

    Fix links on test product page

--- test_product_page.py
+++ test_product_page.py
@@ -2,13 +2,13 @@ import pytest
 import time
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
-from pages.login_page import LoginPage
+from .pages.login_page import LoginPage
 
 
 class TestAddToBasketFromProductPage():
     @pytest.mark.need_review
-    def test_guest_can_add_product_to_basket(browser):
-        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    def test_guest_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
         page.open()
         page.should_be_add_button()
@@ -23,7 +23,7 @@ class TestAddToBasketFromProductPage():
         page.item_price_should_be_equal()
 
     @pytest.mark.xfail
-    def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
         page.open()
@@ -31,14 +31,14 @@ class TestAddToBasketFromProductPage():
         page.solve_quiz_and_get_code()
         page.should_not_be_success_message()
 
-    def test_guest_cant_see_success_message(browser):
+    def test_guest_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
 
     @pytest.mark.xfail
-    def test_message_disappeared_after_adding_product_to_basket(browser):
+    def test_message_disappeared_after_adding_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
         page.open()
@@ -47,7 +47,6 @@ class TestAddToBasketFromProductPage():
         page.success_message_should_disappeared()
 
 
-@pytest.mark.temp_marker
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -59,14 +58,15 @@ class TestUserAddToBasketFromProductPage():
         page.register_new_user(email, password)
         page.should_be_authorized_user()
 
-    def test_user_cant_see_success_message(self, browser, link):
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
 
     @pytest.mark.need_review
-    def test_user_can_add_product_to_basket(browser):
-        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
         page.open()
         page.should_be_add_button()
